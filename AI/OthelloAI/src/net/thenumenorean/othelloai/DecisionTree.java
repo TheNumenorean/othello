50531e46097d71a471c58059ebfa8075502cf6b8
Francesco Macagno <fjmacagno1@yahoo.com>
2016-03-07 17:59:28 -0800

    Simplified Decision Tree

--- AI/OthelloAI/src/net/thenumenorean/othelloai/DecisionTree.java
+++ AI/OthelloAI/src/net/thenumenorean/othelloai/DecisionTree.java
@@ -7,44 +7,94 @@ import net.thenumenorean.othelloai.board.OthelloBoard.OthelloSide;
 
 /**
  * Represents everything discovered by the AI
+ * 
  * @author Francesco
  *
  */
 public class DecisionTree {
-	
-	
 
-	public DecisionTree() {
-		
+	private ConcurrentSkipListSet<DecisionTreeNode> nextMoves;
+	private OthelloSide currentTurn;
+
+	public DecisionTree(OthelloSide currentTurn) {
+		this.currentTurn = currentTurn;
+		nextMoves = new ConcurrentSkipListSet<DecisionTreeNode>();
 	}
-	
+
+	public ConcurrentSkipListSet<DecisionTreeNode> getPossibleNextMoves() {
+		return nextMoves;
+	}
+
+	/**
+	 * Check that the given move is valid. If so, reduce the tree to just
+	 * children of the passed move, since we dont care about the others anymore.
+	 * 
+	 * The given move is assumed to be a move by the current player.
+	 * 
+	 * @param m
+	 *            The move that occured.
+	 */
+	public void moveOccured(Move m) {
+
+		DecisionTreeNode node = null;
+		for (DecisionTreeNode tmp : nextMoves) {
+			if (tmp.move.equals(m)) {
+				node = tmp;
+				break;
+			}
+		}
+
+		if (node == null)
+			throw new InternalError("Move passed that isnt a valid option");
+
+		nextMoves = node.children;
+		currentTurn = currentTurn.opposite();
+
+	}
+
 	/**
 	 * Represents a single move in the tree.
 	 * 
 	 * @author Francesco
 	 *
 	 */
-	public static class DecisionTreeNode implements Comparable<DecisionTreeNode>{
-		
-		//Value is tthe 'smart' valu, score is the actual score at this stage
-		private int value, score;
-		
+	public static class DecisionTreeNode implements Comparable<DecisionTreeNode> {
+
+		/**
+		 * the value this specific move gets
+		 */
+		public int baseValue;
+
+		/**
+		 * the value dependent on possible future moves
+		 */
+		public int smartValue;
+
+		/**
+		 * technical score at this point in the game
+		 */
+		public int score;
+
 		private ConcurrentSkipListSet<DecisionTreeNode> children;
 		private DecisionTreeNode parent;
 
 		private Move move;
 
 		private OthelloSide side;
-		
+
 		public DecisionTreeNode(Move m, OthelloSide side) {
 			children = new ConcurrentSkipListSet<DecisionTreeNode>();
 			this.move = m;
 			this.side = side;
+
+			baseValue = Integer.MIN_VALUE;
+			smartValue = Integer.MIN_VALUE;
+			score = Integer.MIN_VALUE;
 		}
 
 		@Override
 		public int compareTo(DecisionTreeNode o) {
-			return value - o.value;
+			return smartValue - o.smartValue;
 		}
 
 		public DecisionTreeNode getParent() {
@@ -63,26 +113,12 @@ public class DecisionTree {
 		}
 
 		/**
-		 * @return the value
-		 */
-		public int getValue() {
-			return value;
-		}
-
-		/**
-		 * @param value the value to set
-		 */
-		public void setValue(int value) {
-			this.value = value;
-		}
-
-		/**
 		 * @return the move
 		 */
 		public Move getMove() {
 			return move;
 		}
-		
+
 		public void addChild(DecisionTreeNode n) {
 			n.setParent(this);
 			children.add(n);
@@ -95,22 +131,6 @@ public class DecisionTree {
 			return side;
 		}
 
-		/**
-		 * @return the score
-		 */
-		public int getScore() {
-			return score;
-		}
-
-		/**
-		 * @param score the score to set
-		 */
-		public void setScore(int score) {
-			this.score = score;
-		}
-		
 	}
-	
-	
 
 }
